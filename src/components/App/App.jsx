709d9d676b65da0d6b52910e8b4d59fd6f321aee
pthomas0309 <preston.thomas355@gmail.com>
2021-06-06 12:18:23 -0500

    create client side GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList'
+import axios from 'axios';
 
 function App() {
+
+    // State to hold our list data
+    const [galleryList, setGalleryList] = useState([]);
+
+    // run our data getting function on page load
+    useEffect( () => {
+        getGallery();
+    }, [])
+
+    // retrieve gallery from server
+    const getGallery = () => {
+      console.log('Getting the gallery');
+
+      // axios GET to server
+      axios.get('/gallery')
+      // async brings back a response
+      .then(response => {
+
+          // check the response.data for the right info
+          console.log(response.data);
+          // set galleryList to the val of response.data 
+          setGalleryList(response.data);
+      })
+      
+      // catch for error
+      .catch(err => {
+        console.log('Error getting data from the server,', err);
+      })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
+        <GalleryList gallery={galleryList}/>
         {/* <img src="images/goat_small.jpg"/> */}
       </div>
     );
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,12 @@
+function GalleryList() {
+    // Gallery list houses the GalleryItem components
+    return (
+        <>
+            <section>
+
+            </section>
+        </>
+    )
+}
+
+export default GalleryList;
