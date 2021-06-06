2c5bd2c939c10974200edbc1f7096549739737fd
pthomas0309 <preston.thomas355@gmail.com>
2021-06-06 05:02:32 -0500

    filled galleryItems with the gallery obj. info

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/wicked.jpg', description: 'Stage of Broadway musical, Wicked moments before opening act. A moment captured on the photographers\'s first adventure to New York.', medium: 'iPhone 8', fingerprints: 0 },
+    { id: 2, path: 'images/concert.jpg', description: 'Tyler the Creator in the middle of a set for his album Flower Boy. This photo was captured during a memorable concert experience.', medium: 'iPhone 7', fingerprints: 0 },
+    { id: 3, path: 'images/denim.jpg', description: 'A pair of denimn jeans on a teal backdrop. Must be a contemporary piece?', artist: 'Wrangler Jeans', medium: 'cyan asphault', fingerprints: 0 },
+    { id: 4, path: 'images/doggo.jpg', description: 'Suprise cuddles from a chihuahua. Captured on the tender meeting of a new friend.', medium: 'iPhone SE', fingerprints: 0 },
+    { id: 5, path: 'images/flat.jpg', description: 'Pieces of a (mostly) intact spare tire. Slightly rusted in a tragicakky pretty way.', medium: 'metal and rubber', fingerprints: 0 },
+    { id: 6, path: 'images/mural.jpg', description: 'A mural displayed in the Kansas City plaza district. This image was captured during the protests against police violence. The artist of the mural is unknown.', medium: 'spraypaint on plywood billboards', fingerprints: 0 },
+    { id: 7, path: 'images/sfx.jpg', warning: 'Injury created with special effects (safe to touch...if you dare)', description: 'Make-up simulation of a split knuckle accident. This exhibit was inspired by a workshop with a make-up artist working with emergency responders in re-creating injuries for disaster drills.', medium: 'Mehron SynthWax, fake blood (mint flavored)', fingerprints: 0 },
+    { id: 8, path: 'images/sleepy.jpg', description: 'A very sleepy kitty. Loves hooman pets and dislikes asking twice.', medium:'fur, snapchat filter', pets: 0 },
+    { id: 9, path: 'images/streetsNY.jpg', description: 'The Streets of New York. The photographer reports to have been "walkin\' he-uh"', medium: "iPhone 8", fingerprints: 0 }
 ];
 
 module.exports = galleryItems;
